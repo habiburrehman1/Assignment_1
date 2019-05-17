c6e97ce155a90b342897c9e6c6442eb6388f7469
habiburrehman1 <50754292+habiburrehman1@users.noreply.github.com>
2019-05-18 01:42:49 +0500

    Add files via upload

---
+++ grading.py
@@ -0,0 +1,21 @@
+english = int(input("marks of english:"))
+biology = int(input("marks of biology:"))
+islamiat = int(input("marks of islamiat:"))
+urdu = int(input("marks of urdu:"))
+math = int(input("marks of math:"))
+marks_obtained = english + biology + islamiat + urdu + math
+print(marks_obtained)
+percentage = (marks_obtained/500*100)
+print(percentage)
+if (percentage >= 85) :
+    print("grade A+1")
+elif (percentage >= 70) :
+    print("grade A")
+elif (percentage >= 60) :
+    print("grade B")
+elif (percentage >= 50) :
+    print("grade C")
+elif (percentage >= 40) :
+    print("grade D")
+else :
+    print("fail")
